6012a80927a5734d7cdd9584788a4980c1fca25d
brycehills <hillsbryce@yahoo.com>
2021-01-10 13:00:00 -0800

    added next section of init step 4

--- cracker.py
+++ cracker.py
@@ -4,16 +4,29 @@ import hashlib #library to implement md5
 def encrypt(pw, salt):
 
 	#initialization
-	alternateSum = pw + salt + pw
-	alternateSum = hashlib.md5(alternateSum.encode('utf-8')).hexdigest()
-
 	
+	#get altsum
+	alternateSum = pw + salt + pw
+	hashedAlternateSum = hashlib.md5(alternateSum.encode('utf-8')).hexdigest()
+
 	#compute the intermidiate sum
 	intermidiateSum = pw + magic + salt
-	intermidiateSum = hashlib.md5(intermidiateSum.encode('utf-8')).hexdigest()
 	
-	print("alternate sum: " + alternateSum + "\n")
-	print("intermidate sum: " + intermidiateSum + "\n")
+	# get len(pw) in bytes
+	passwordLength = len(pw.encode('utf-8'))
+	
+	# add length(password) bytes of the Alternate sum, repeated as necessary
+	# i am confused here -- it seems we are concatenating the len(pw) in bytes until we finish the whole altsum
+	for i in range(passwordLength):
+		intermidiateSum += hashedAlternateSum
+	
+	
+	# hash final concatenated version of int sum
+	hashedIntermidiateSum = hashlib.md5(intermidiateSum.encode('utf-8')).hexdigest()
+	
+	# testing purposes
+	print("hashed alternate sum: " + hashedAlternateSum + "\n")
+	print("hashed intermidate sum: " + hashedIntermidiateSum + "\n")
 
 #define magic
 magic = "$1$"
