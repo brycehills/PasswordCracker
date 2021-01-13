19cd3ad13e62947bd944066da62854cf76113255
brycehills <hillsbryce@yahoo.com>
2021-01-13 14:51:53 -0800

    Update cracker.py

--- cracker.py
+++ cracker.py
@@ -1,5 +1,5 @@
 import sys
-import hashlib #library to implement md5
+import hashlib  # library to implement md5
 import binascii # for hex conversion
 
 def encrypt(pw, salt, magic):
@@ -7,38 +7,36 @@ def encrypt(pw, salt, magic):
 	#*******************************initialization******************************
 	alt = pw + salt + pw        #get alt
 	res = pw + magic + salt		#compute res
-	print("res = " + res)
+	print("res = " + str(res))
 	
-	h = hashlib.md5(alt.encode()).hexdigest()
-	print("h= " + h)
+	h = hashlib.md5(alt).digest()
+	print("h = " + str(h))
 	
 	l = len(pw)	# get len(pw) -- should be 6 in our case
 	print("l = " + str(l))
 	
-	while l > 0:
+	while l > 0: #concat h to res for len(l)
 		res = res + h[0:min(16,l)]
 		l = l - 16
 		
-	print("\nres after 1st while loop = " + res)
-	print("in hex: " + str(res.encode().hex()))
+	print("\nres after 1st while loop = " + str(res))
 	print("\n")
 	
 	l = len(pw) #reset l
 	# For each bit in length(password), from low to high 
 	while l:
 		if l & 1:
-			res += '\0' # append null chr
+			res += b'\x00' # append null chr
 		else:
 			res += pw[0:1] # append first bit of pw
 		l>>= 1
 		
-	print("res after 2nd while loop = " + res)
-	print("in hex: " + str(res.encode().hex()))
+	print("res after 2nd while loop = " + str(res))
 	print("\n")
 
-	hashedres = hashlib.md5(res.encode()).hexdigest() # hash concatenated version of res
+	hashedres = hashlib.md5(res).digest() # hash concatenated version of res
 	
-	print("hashedres = " + hashedres)
+	print("hashedres = " + str(hashedres))
 	#***************************************************************************
 	
 	
@@ -46,7 +44,7 @@ def encrypt(pw, salt, magic):
 	#********************************** LOOP ***********************************
 	
 	for i in range(1000):
-		tmp = "" 					# tmp str
+		tmp = b"" 					# tmp str
 		if i&1: tmp += pw
 		else: tmp += hashedres # should this be hashed version or regular string?
 		
@@ -57,18 +55,20 @@ def encrypt(pw, salt, magic):
 		if i&1: tmp += hashedres
 		else: tmp += pw
 		
-		hashedres = hashlib.md5(tmp.encode()).hexdigest()
+		hashedres = hashlib.md5(tmp).digest()
 	#***************************************************************************
 	
 	
 	# testing purposes
-	print("hashedres: " + hashedres + "\n")
+	print("hashedres: " + str(hashedres) + "\n")
 
 #define magic
-magic = "$1$"
+magic = b'$1$'
+pw=b'\x7a\x68\x67\x6e\x6e\x64'
+salt=b'\x68\x66\x54\x37\x6a\x70\x32\x71'
 # prompt user for input pw and salt
-pw = input("Enter password: ")
-salt = input("Enter salt: ")
+#pw = input("Enter password: ")
+#salt = input("Enter salt: ")
 print("\n")
 encrypt(pw,salt,magic)
 
