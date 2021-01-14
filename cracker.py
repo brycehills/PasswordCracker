047647f735f0d842d1eecbd36fff5ed709ccb19b
brycehills <hillsbryce@yahoo.com>
2021-01-14 13:12:06 -0800

    all hashing/logic working properly up to loop function

--- cracker.py
+++ cracker.py
@@ -22,7 +22,7 @@ def encrypt(pw, salt, magic):
 	print("\nres after 1st while loop = " + str(res))
 	print("\n")
 	
-	res1comp = b'x7a\x68\x67\x6e\x6e\x64\x24\x31\x24\x68\x66\x54\x37\x6a\x70\x32\x71\x3f\xfc\x86\xe7\xc7\x8f'
+	res1comp = b'\x7a\x68\x67\x6e\x6e\x64\x24\x31\x24\x68\x66\x54\x37\x6a\x70\x32\x71\x3f\xfc\x86\xe7\xc7\x8f'
 	
 	if res == res1comp:
 		print('res match')
@@ -33,11 +33,18 @@ def encrypt(pw, salt, magic):
 	l = len(pw) #reset l
 	# For each bit in length(password), from low to high bits
 	while l:
+		print("l= " + str(l))
 		if l & 1:
 			res += b'\x00' # append null chr
 		else:
 			res += pw[0:1] # append first bit of pw
 		l>>= 1 #shiftleft
+		
+	while2res = b'\x7a\x68\x67\x6e\x6e\x64\x24\x31\x24\x68\x66\x54\x37\x6a\x70\x32\x71\x3f\xfc\x86\xe7\xc7\x8f\x7a\x00\x00'
+	if res == while2res:
+		print('while2res match')
+	else:
+		print('res no match for while2res')	
 		
 	print("res after 2nd while loop = " + str(res))
 	print("\n")
@@ -47,9 +54,9 @@ def encrypt(pw, salt, magic):
 	print("hashedres = " + str(hashedres))
 	
 	# test strings
-	compRes = b'xed\x7a\x53\x07\x58\x8e\x49\xed\x3a\x27\x77\xd9\x26\xd6\x2f\x96'
+	compRes = b'\xed\x7a\x53\x07\x58\x8e\x49\xed\x3a\x27\x77\xd9\x26\xd6\x2f\x96'
 	
-	if res == compRes:
+	if hashedres == compRes:
 		print('match')
 	else:
 		print('no match')
@@ -74,9 +81,11 @@ def encrypt(pw, salt, magic):
 		hashedres = hashlib.md5(tmp).digest()
 	#***************************************************************************
 	
-	
-	# testing purposes
-	print("hashedres: " + str(hashedres) + "\n")
+	finalrescomp = b'\xff\x20\x2f\x2e\x9b\x6a\xc6\xe4\x95\x57\x05\x36\xfc\x89\xfd\x2a'
+	if hashedres == finalrescomp:
+		print('final match')
+	else:
+		print('no final match')
 
 #define magic
 magic = b'$1$'
